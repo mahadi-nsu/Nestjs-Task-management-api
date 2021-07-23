dcf7d599bde6d3c7a3f87a04a1adf5ae6a6ff778
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-07-23 21:22:07 +0600

    Error handeling added for single task not found

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task-dto';
@@ -40,7 +40,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task ${id} not found`);
+    }
+    return found;
   }
 
   deleteTaskById(id: string): void {
